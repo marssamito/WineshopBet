11862b3cb0bcca20b2dd4bd0bc6262d4a8dfe200
marssamito <mwangisam671@gmail.com>
2022-09-21 09:32:45 +0300

    Add files via upload

---
+++ nuxt.config.js
@@ -0,0 +1,81 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  target: 'static',
+  head: {
+    target: 'static',
+    title: 'Wine Shop',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+ // generate: {
+   // routes:[
+   //   'checkout',
+   //   'details',
+  //    'cart',
+      
+
+
+  //  ]
+ // },
+
+  // runtime config
+  publicRuntimeConfig: {
+    apiURL: process.env.API_URL,
+    imgBaseUrl: process.env.IMG_BASE_URL || '',
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    '~/assets/scss/global.scss',
+    'ant-design-vue/dist/antd.css'
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    '@/plugins/antd-ui',
+    '~/plugins/draggable',
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: {
+    dirs: ['~/components','~/components/modal'],
+  },
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/google-fonts',
+    '@nuxtjs/moment',
+  ],
+  googleFonts: {
+    families: {
+      Lato: [100, 300, 400, 700, 900],
+    },
+    display: 'swap',
+    download: true,
+  },
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // proxy: true,
+    baseURL: process.env.API_URL,
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+}
